da3244f5d1d993b9883ab19d34bf15db8ce0ce82
XuanLanhbk <55599200+XuanLanhbk@users.noreply.github.com>
2020-03-16 12:20:33 +0700

    ゲエン．スアン．ライン(20152110)
    問題１．

--- example.js
+++ example.js
@@ -3,3 +3,10 @@
 'use strict';
 
 // このファイルを修正して、プルリクエストしてください。
+var array = [];
+for (const arg of process.argv){
+    array.push(arg)
+}
+for(var i = array.length; i >= 0; i--){
+    console.log(i + "番目は" + array[i] + 'です');
+}
